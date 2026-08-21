2f449bef792c0e9ca4abedc628df48cf66ce73b9
CHA ZI YU <23120943@siswa.um.edu.my>
2026-08-21 15:14:01 +0800

    fix error

--- fix.py
+++
@@ -1,9 +0,0 @@
-import sys
-with open('src/features/access/access-template.js', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('\\"', '"').replace('\\n', '\n')
-
-with open('src/features/access/access-template.js', 'w', encoding='utf-8') as f:
-    f.write(content)
-print('Done!')
